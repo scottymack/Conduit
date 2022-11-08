fa3fcf324a42e9df61dac5ee4a04bcbf16127bc0
dimitris <dimitrissoldatos2@gmail.com>
2022-11-08 13:52:18 +0200

    refactor(communicator): sms,email,push notifications removed

--- modules/email/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { ModuleManager } from '@conduitplatform/grpc-sdk';
-import EmailModule from './Email';
-import { Config } from './config';
-const email = new EmailModule();
-const moduleManager = new ModuleManager<Config>(email);
-moduleManager.start();
--- modules/push-notifications/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { ModuleManager } from '@conduitplatform/grpc-sdk';
-import PushNotificationsModule from './PushNotifications';
-import { Config } from './config';
-
-const pushNotifications = new PushNotificationsModule();
-const moduleManager = new ModuleManager<Config>(pushNotifications);
-moduleManager.start();
